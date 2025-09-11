b2b28a5f0b577a0d0136416a7b88c038f7e357b7
besmirguma <besmir@metanow.dev>
2025-09-11 14:26:05 +0200

    solving template errors

--- resources/views/livewire/templates-index.blade.php
+++ resources/views/livewire/templates-index.blade.php
@@ -493,14 +493,14 @@
                 </div>
             </div>
         @endif
-    </div>
-
-    <!-- Footer -->
-    <footer class="bg-white border-t border-gray-200 mt-12">
-        <div class="container mx-auto px-4 py-6">
-            <div class="text-center text-gray-600">
-                <p>&copy; {{ date('Y') }} Metanow. All rights reserved.</p>
+        
+        <!-- Footer -->
+        <footer class="bg-white border-t border-gray-200 mt-12">
+            <div class="container mx-auto px-4 py-6">
+                <div class="text-center text-gray-600">
+                    <p>&copy; {{ date('Y') }} Metanow. All rights reserved.</p>
+                </div>
             </div>
-        </div>
-    </footer>
+        </footer>
+    </div>
 </div>
